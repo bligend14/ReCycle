60ea5d23fed58dfe87d42bdc1e80e9a7c2d7251f
DESKTOP-CGD8L5N\rlawo <rlawodmsajtw0@gmail.com>
2021-09-30 19:34:17 +0900

    회원정보 입력 기능 추가

--- app/src/main/java/com/example/re_cycle/MainActivity.java
+++ app/src/main/java/com/example/re_cycle/MainActivity.java
@@ -3,10 +3,14 @@ package com.example.re_cycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserInfo;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -16,10 +20,32 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
-        if (FirebaseAuth.getInstance().getCurrentUser() == null)
+
+        if (user == null)
         {
-            GotologinActivity();
+           GotoActivity(loginActivity.class);
+        }
+        else
+        {
+            for (UserInfo profile : user.getProviderData())
+            {
+                // Name, email address, and profile photo Url
+                String name = profile.getDisplayName();
+                Log.e("이름","이름:"+ name);
+                if (name != null)
+                {
+                    if (name.length() == 0)
+                    {
+                        GotoActivity(MemberinitActivity.class);
+                    }
+                }
+                else if (name == null)
+                {
+                    GotoActivity(MemberinitActivity.class);
+                }
+            }
         }
 
         findViewById(R.id.logoutButton).setOnClickListener(onClickListener);
@@ -35,15 +61,16 @@ public class MainActivity extends AppCompatActivity
             {
                 case R.id.logoutButton:
                     FirebaseAuth.getInstance().signOut();
-                    GotologinActivity();
+                    GotoActivity(loginActivity.class);
                     break;
             }
         }
     };
 
-    private void  GotologinActivity()
+
+    private void  GotoActivity(Class I)
     {
-        Intent intent = new Intent(this,loginActivity.class);
+        Intent intent = new Intent(this,I);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
     }
